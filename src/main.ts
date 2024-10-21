72c1b024164625c687bfe2ef376965a2f4eae67c
GannaHup <gannahap02@gmail.com>
2024-10-21 08:16:37 +0700

    fix: update config enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,11 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
+    exposedHeaders: 'Authorization',
   });
 
   app.useGlobalPipes(new ValidationPipe()); // Enable validation globally
