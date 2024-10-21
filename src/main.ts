a706494479680cb9cd874ce3428f2393621807d2
GannaHup <gannahap02@gmail.com>
2024-10-21 07:52:48 +0700

    fix: update port

--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,10 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
