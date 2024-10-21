84f7e1182729249759dad3e336292110c168d4e6
GannaHup <gannahap02@gmail.com>
2024-10-21 18:59:58 +0700

    fix: update config enable cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000'], // Replace with actual frontend URLs
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Origin,Content-Type,Accept,Authorization',
     credentials: true, // Include credentials if needed (cookies, etc.)
     exposedHeaders: 'Content-Length,Content-Type,Authorization',
