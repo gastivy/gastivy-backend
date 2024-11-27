2b4ec952e1a4da961506d3faa9569a5d1682673d
GannaHup <gannahap02@gmail.com>
2024-11-27 21:00:50 +0700

    fix: [GAS-35] CORS allowed

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with actual frontend URLs
+    origin: ['http://localhost:3000', 'https://gastivy.netlify.app'], // Replace with actual frontend URLs
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     credentials: true, // Include credentials if needed (cookies, etc.)
   });
